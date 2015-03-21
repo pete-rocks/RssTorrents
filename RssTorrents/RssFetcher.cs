1aaa459cbc384f319516cd56418c58fa5029fadd
pete-rocks <deaneyRocks@gmail.com>
2015-03-21 12:15:05 +0800

    Initial commit

---
+++ RssTorrents/RssFetcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace RssTorrents
+{
+	public class RssFetcher
+	{
+		public RssFetcher ()
+		{
+		}
+
+		public bool RssFeedExists(string feedUrl)
+		{
+			WebRequest request = WebRequest.Create(feedUrl);
+
+			var response = request.GetResponse ();
+
+			Stream dataStream = response.GetResponseStream ();
+
+
+			response.Close();
+
+		}
+
+	}
+}
+
